5d4c0fc8dc362d70f6467e19a8b473452d862101
himanshu <himanshu.sanecha@tikaj.com>
2023-07-29 19:47:24 +0530

    update: make two separate apps for frontend and backend.

--- backend/index.js
+++ backend/index.js
@@ -6,20 +6,10 @@ var cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 8000;
 
-const pathToBuild = "./../frontend/build/";
-
-var history = require("connect-history-api-fallback");
-app.use(history());
-app.use(express.static(path.join(__dirname, pathToBuild)));
-
 // Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
-
-app.get(/^(?!\/api\/)/, (_, res) => {
-  res.sendFile(path.join(__dirname, pathToBuild, "index.html"));
-});
 
 app.use("/api/user", require("./Auth/loginRoute"));
 app.use("/api/camera", require("./Auth/cameraRoute"));
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-const API = "/api";
+const API = "http://localhost:8080/api";
 //const API = "https://camera-streaming-backend.onrender.com";
 module.exports = { API };
