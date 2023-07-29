45dd1add3bc9c7c40dbbc5b111a3b2c11934374d
himanshu <himanshu.sanecha@tikaj.com>
2023-07-29 15:15:55 +0530

    update: make nodejs render react build.

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,27 @@ const express = require("express");
 const app = express();
 require("./db/conn");
 var cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 const port = process.env.PORT || 8000;
+
+const path = "./../frontend/build/";
+
+var history = require("connect-history-api-fallback");
+app.use(history());
+app.use(express.static(path));
+
+app.get("/", function (req, res) {
+  res.sendFile(path + "index.html");
+});
 
 // Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.use("/user",require("./Auth/loginRoute"));
-app.use("/camera",require("./Auth/cameraRoute"));
+app.use("/user", require("./Auth/loginRoute"));
+app.use("/camera", require("./Auth/cameraRoute"));
 
-app.listen(port,()=>{
-    console.log(`listening the port successfully on port: ${port}`);
-})
+app.listen(port, () => {
+  console.log(`listening the port successfully on port: ${port}`);
+});
