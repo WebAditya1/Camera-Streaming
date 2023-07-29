d48dc291ba3899fcda5c05f909322845c46f2bf2
himanshu <himanshu.sanecha@tikaj.com>
2023-07-29 20:13:58 +0530

    update: use aws url.

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-const API = "http://localhost:8080/api";
+const API = "http://ec2-16-171-39-42.eu-north-1.compute.amazonaws.com:8080/api";
 //const API = "https://camera-streaming-backend.onrender.com";
 module.exports = { API };
