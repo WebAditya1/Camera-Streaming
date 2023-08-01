359ed90c58687d04a5ee93fe8a12fe741b13fe26
Himanshu Sanecha <52252631+himanshusanecha@users.noreply.github.com>
2023-08-01 18:55:43 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-const API = "http://ec2-16-171-39-42.eu-north-1.compute.amazonaws.com:8080/api";
+const API = " ec2-16-16-245-42.eu-north-1.compute.amazonaws.com:4000/api";
 //const API = "https://camera-streaming-backend.onrender.com";
 module.exports = { API };
