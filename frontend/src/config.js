609975dd094c99c2acea3474fab8af94b8aa1909
himanshu <himanshu.sanecha@tikaj.com>
2023-08-01 20:19:24 +0530

    update: api url.

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-const API = " ec2-16-16-245-42.eu-north-1.compute.amazonaws.com:4000/api";
+const API = "http://localhost:4000/api";
 //const API = "https://camera-streaming-backend.onrender.com";
 module.exports = { API };
